0e32016d47a13c25da60498581b472c4774a20eb
RaengC <raengc@yahoo.com>
2020-11-06 21:48:34 +1100

    support options added and showing

---
+++ src/components/ChatBot/SupportOptions/SupportOptions.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './SupportOptions.css';
+
+const SupportOptions = (props) => {
+  const options = [
+    {text: "Meditation", handler: () => {}, id: 1},
+    {text: "Mindfullness", handler: () => {}, id: 2},
+    {text: "Breathing", handler: () => {}, id: 3},
+    {text: "Creative Outlets", handler: () => {}, id: 4},
+    {text: "Anxiety Resources", handler: () => {}, id: 5},
+  ];
+
+  const optionsMarkup = options.map((option) => (
+    <button
+      className="support-option-button"
+      key={option.id}
+      onClick={option.handler}
+    >
+      {option.text}
+    </button>
+  ));
+  return <div className="support-options-container">{optionsMarkup}</div>;
+
+}
+
+export default SupportOptions
--- src/components/ChatBot/config.js
+++ src/components/ChatBot/config.js
@@ -1,7 +1,22 @@
+import React from 'react';
 import { createChatBotMessage } from 'react-chatbot-kit';
 
+import SupportOptions from './SupportOptions/SupportOptions';
+
 const config = {
-  initialMessages: [createChatBotMessage('Hello World')]
-}
+  initialMessages: [
+    createChatBotMessage("Hi, I'm here to help. What what would you like help with?", {
+      widget: "supportOptions",
+    }),
+  ],
+
+  widgets: [
+    {
+      widgetName: "supportOptions",
+      widgetFunc: (props) => <SupportOptions {...props} />
+    }
+  ],
+
+};
 
 export default config;
