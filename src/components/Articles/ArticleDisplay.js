5e740702e9fe493a87982dc6891a929e8bc524f4
Brittany Crocker <brittany.crocker@outlook.com>
2020-11-08 14:38:10 +1100

    ArticleComments displaying, state and props passing with ArticleComments & SavedComments

--- src/components/Articles/ArticleDisplay.js
+++ src/components/Articles/ArticleDisplay.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import ArticleComments from './ArticleComments'
+import DisplayComments from './DisplayComments'
+
+
 
 class ArticleDisplay extends Component {
   constructor() {
     super()
     this.state = {
       articleName: 'Yoga Nidra',
-      articleBody: 'Around the room, faces relax, jaws soften, and soon snores start to rumble as the men drop deeper into relaxation.'
+      articleBody: 'Around the room, faces relax, jaws soften, and soon snores start to rumble as the men drop deeper into relaxation.',
+      allComments: ['hi', 'there']
     }
+      this._handleComments = this._handleComments.bind(this)
+  }
 
+  _handleComments(comments) {
+    this.setState({allComments: [... this.state.allComments, comments ]})
   }
   render() {
     return (
@@ -15,10 +24,18 @@ class ArticleDisplay extends Component {
       <h1> {this.state.articleName} </h1>
       <p> {this.state.articleBody} </p>
       <button>Save {this.state.articleName} Article</button>
+      <ArticleComments onSubmit={this._handleComments}/>
+      <ul>
+      {this.state.allComments.map((comment) =>
+        <li>{comment}</li>
+      )}
+      </ul>
       </div>
+
     )
   }
 }
+
 
 
 
