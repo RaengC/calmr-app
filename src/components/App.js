cd4646d39f8a9ecb13d0a8b2c9f58883660d9b33
RaengC <raengc@yahoo.com>
2020-11-08 21:21:21 +1100

    pop up for chat, alignment sorted

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Chatbot from 'react-chatbot-kit'
+import { ConditionallyRender } from "react-util-kit";
 
 import BotApp from './ChatBot/BotApp';
 import JournalLanding from './Journal/JournalLanding';
 import ArticleDisplay from './Articles/ArticleDisplay';
 import SavedArticles from './Articles/SavedArticles';
-import Test from './Test'
+
+import { ReactComponent as ButtonIcon } from "./Assets/lotus-flower.png";
+
+import '../App.css';
 
 function App() {
+  const [showChatBot, toggleChatbot] = useState(true);
+
   return (
     <div className="App">
       <JournalLanding />
-      <BotApp />
       <ArticleDisplay/>
       <SavedArticles/>
+      <div className="app-chatbot-container">
+        <ConditionallyRender
+          ifTrue={showChatBot}
+          show={ <BotApp /> }
+        />
+      </div>
+
+      <button
+        className="app-chatbot-button">
+        <img
+
+          src={require('./Assets/lotus-flower.png')}
+          onClick={() => toggleChatbot((prev) => !prev)} />
+      </button>
+
 
     </div>
   );
