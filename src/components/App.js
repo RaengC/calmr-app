22fa469dafa2b16fe34680b5ee3468f3084f792c
RaengC <raengc@yahoo.com>
2020-11-08 15:43:55 +1100

    update merge

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD
 
 import BotApp from './ChatBot/BotApp';
 import JournalLanding from './Journal/JournalLanding';
-=======
 import Test from './Test'
-import BotApp from './ChatBot/BotApp'
->>>>>>> 5e740702e9fe493a87982dc6891a929e8bc524f4
 
 function App() {
   return (
     <div className="App">
       <JournalLanding />
       <BotApp />
-<<<<<<< HEAD
-=======
       <Test />
->>>>>>> 5e740702e9fe493a87982dc6891a929e8bc524f4
 
     </div>
   );
