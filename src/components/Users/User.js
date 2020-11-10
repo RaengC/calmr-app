396b9c204613510df095bc39edf3ca0fee14c22c
lightgreener <woniora@hotmail.com>
2020-11-11 08:25:28 +1100

    pull data

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -8,16 +8,49 @@ class User extends Component {
   constructor() {
     super();
     this.state = {
-
+      isLoggedIn: false,
+      user: {}
     }
-}
+  }
+
+  componentDidMount() {
+    this.loginStatus()
+  }
+
+  loginStatus = () => {
+  axios.get('http://localhost:3001/logged_in',
+ {withCredentials: true})
+  .then(response => {
+    if (response.data.logged_in) {
+      this.handleLogin(response)
+    } else {
+      this.handleLogout()
+    }
+  })
+  .catch(error => console.log('api errors:', error))
+ }
+
+  handleLogin = (data) => {
+      this.setState({
+        isLoggedIn: true,
+        user: data.user
+      })
+    }
+  handleLogout = () => {
+      this.setState({
+      isLoggedIn: false,
+      user: {}
+      })
+    }
+
   render () {
     return (
       <div className="loginPage">
         <BrowserRouter>
           <Switch>
-            <Route exact path={"/login/UserHome"} component={UserHome} />
-            <Route exact path={"/login/UserDashboard"} component={UserDashboard}/>
+              <Route exact path='/' component={}/>
+              <Route exact path='/login' component={}/>
+              <Route exact path='/signup' component={}/>
 
          </Switch>
         </BrowserRouter>
