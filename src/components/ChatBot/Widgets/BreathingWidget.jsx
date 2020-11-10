234dd171be24fe964d26555274cb02fa1443a824
Brittany Crocker <brittany.crocker@outlook.com>
2020-11-10 13:38:51 +1100

    All widgets transformed to stateful components

--- src/components/ChatBot/Widgets/BreathingWidget.jsx
+++ src/components/ChatBot/Widgets/BreathingWidget.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import optionsMarkup from '../WidgetsList/OptionsMarkup';
+import OptionsMarkup from '../WidgetsList/OptionsMarkup';
 
-const BreathingWidget = (props) => {
-  const options = [
-    {
+
+class BreathingWidget extends Component {
+  constructor(props){
+  super(props)
+  this.state = {
+    options: [{
       text: "Breathing to Stop Anxiety Attack",
       url: "https://youtu.be/vXZ5l7G6T2I",
       id: 1,
@@ -23,11 +26,14 @@ const BreathingWidget = (props) => {
       text: "Mindful Breathing",
       url: "https://youtu.be/nmFUDkj1Aq0",
       id: 4,
-    },
-  ];
-
-  return <ul className="link-list">{optionsMarkup(options)}</ul>;
+    }]
+  }
+  }
+  render(){
+    return (
+      <ul className="link-list"><OptionsMarkup options={this.state.options}/></ul>
+    )
+  }
 }
-
 
 export default BreathingWidget;
